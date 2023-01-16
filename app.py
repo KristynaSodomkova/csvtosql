629756db1283a77cf9643eb2995fc598bcdb6fbf
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-01-16 22:21:38 +0100

    changed how the file is decoded (utf-8, split), removed else statement

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import csv
 
 app = Flask(__name__)
 
+
+# app.config['DEBUG'] = True
 # make the data from tuple into dict
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
 
 # display the data on page
 @app.route('/transactions', methods=['GET', 'POST'])
@@ -22,10 +25,11 @@ def display_transactions():
         connection.row_factory = dict_factory
         cursor = connection.cursor()
 
-        # read the csv file and insert the data into the database
+        # read the csv file and insert the data into the database, print how many records were added
         records = 0
-        reader = csv.reader(file)
+        reader = file.readlines()
         for row in reader:
+            row = row.decode("utf-8").split(',')
             cursor.execute("INSERT OR REPLACE INTO TransactionsTable VALUES (?,?,?,?,?)", row)
             connection.commit()
             records += 1
@@ -34,27 +38,31 @@ def display_transactions():
         cursor.execute("DELETE from TransactionsTable WHERE reference='reference' ")
         connection.commit()
 
-        # close the cursor and connection
-        cursor.close()
-        connection.close()
-
-        #return a response indicating that the file has been successfully uploaded
-        print("\n{} Records Transferred".format(records))
-
-    else:
-        # select all data from the table and fetch them into results
-        connection = sqlite3.connect("banking.db")
-        connection.row_factory = dict_factory
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM TransactionsTable ORDER BY timestamp DESC")
+        # fetch the results
         results = cursor.fetchall()
 
         # close the cursor and connection
         cursor.close()
         connection.close()
 
-        # pass the results to the template
+        # return a response indicating that the file has been successfully uploaded
+        print("\n{} Records Transferred".format(records))
         return render_template('transactions.html', transactions=results)
+
+    # select all data from the table and fetch them into results
+    connection = sqlite3.connect("banking.db")
+    connection.row_factory = dict_factory
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM TransactionsTable ORDER BY timestamp DESC")
+    results = cursor.fetchall()
+
+    # close the cursor and connection
+    cursor.close()
+    connection.close()
+
+    # pass the results to the template
+    return render_template('transactions.html', transactions=results)
+
 
 if __name__ == '__main__':
     app.run()
